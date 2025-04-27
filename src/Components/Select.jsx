80722e8f5ff16be00f18eb7fe1675722ffdf6ce2
Harsh Banka <harshbanka321@gmail.com>
2025-04-28 01:13:00 +0530

    Update API endpoint in Select component to use environment variable for backend URL

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -19,7 +19,7 @@ function Select(props) {
       interests: selectedButtonNames,
     };
     console.log(data);
-    fetch("http://localhost:5050/api/predict", {
+    fetch(`${import.meta.env.VITE_BACKEND_ENDPOINT}/api/predict`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
